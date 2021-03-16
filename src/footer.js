df633cc822346a1b2e5dfc996b0494326abe493d
Chance Harmon <henrikillsit@gmail.com>
2021-03-15 20:15:19 -0700

    finished with lab1 plus loop stretch

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>
+          By Chance Harmon, &copy; 2021
+        </h2>
+      </>
+    )
+  }
+
+}
+
+export default Footer;
