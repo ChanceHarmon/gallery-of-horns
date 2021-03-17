bcdcb1bc87eddbe13cd6156d1dca669b74f9fce9
Chance Harmon <henrikillsit@gmail.com>
2021-03-16 22:28:02 -0700

    feature 1 and 2 of lab 2 complete. feature technically started with bootstrap, but it doesnt look good yet

--- src/hornedBeasts.js
+++ src/hornedBeasts.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positiveVotes: 0
+    }
+  }
+
+  addVotes = () => {
+    this.setState({ positiveVotes: this.state.positiveVotes + 1 })
+  }
   render() {
     return (
       <>
-        <h2>
-          {this.props.title}
-        </h2>
-        <img src={this.props.url} alt={this.props.description} title={this.props.title} />
-        <p>
-          {this.props.description}
-        </p>
+        <Card className="bg-dark text-white">
+          <Card.Img src={this.props.url} alt={this.props.description} />
+          <Card.ImgOverlay>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              🖤  {this.state.positiveVotes}
+            </Card.Text>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
       </>
     )
   }
